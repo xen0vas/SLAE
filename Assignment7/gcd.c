d2648196e0a7f706acaa258064654be7e2ba29d1
root <root@localhost.localdomain>
2019-05-14 07:13:32 -0400

    gcd added

---
+++ Assignment7/gcd.c
@@ -0,0 +1,29 @@
+#include <stdio.h>  
+
+// Recursive function to return gcd of a and b 
+int gcd(int a, int b)
+{
+  // check for 0 values
+  if (a == 0)
+     return b;
+  if (b == 0)
+     return 0;
+  if (a == b)
+     return a;
+  if (a > b)
+     return gcd(a-b, b);
+  return gcd(a, b-a);
+}
+
+int main()
+{  
+int res = 0;
+int i;        
+for ( i = 1; i<129; i++)
+{
+    res = gcd(i, 128);
+    if (res == 1)
+      printf("%d, ", i);
+}
+return 0;
+}
