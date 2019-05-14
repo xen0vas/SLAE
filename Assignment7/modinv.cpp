a1fa4728496dd48f589287b5d95372eec22eb428
root <root@localhost.localdomain>
2019-05-14 10:10:34 -0400

    modinv.cpp added

---
+++ Assignment7/modinv.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+// A method to find multiplicative inverse of
+// 'a' under modulo 'm' 
+
+int modInverse(int a, int m)
+{
+a = a % m;
+for (int x=1; x<m; x++)
+if ((a*x) % m == 1)
+return x;
+}
+
+int main(int argc, char **argv)
+{
+int a = atoi(argv[1]);
+int m = atoi(argv[2]);
+cout << "\nDecryption Key is: "<< modInverse(a, m) << "\n\n";
+return 0;
+}
